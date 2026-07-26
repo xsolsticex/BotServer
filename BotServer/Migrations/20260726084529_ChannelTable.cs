7890133073176955d591b859df218bc988f2e80d
xsolsticex <carlos_007_34@outlook.com>
2026-07-26 10:46:27 +0200

    add migration

---
+++ BotServer/Migrations/20260726084529_ChannelTable.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BotServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChannelTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JoinedChannels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChannelName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JoinedChannels", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JoinedChannels_ChannelName",
+                table: "JoinedChannels",
+                column: "ChannelName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JoinedChannels");
+        }
+    }
+}
